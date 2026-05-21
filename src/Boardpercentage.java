94f5545d12f87bd04c311fd0b180492153274c86
Prince-30502 <mrprincex30502@gmail.com>
2026-05-21 12:50:15 +0530

    Added Hello World

---
+++ src/Boardpercentage.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+
+    public class Boardpercentage {
+        public static void main(String[] args){
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Enter marks of subject 1:");
+            float sub1 = sc.nextFloat();
+
+            System.out.println("Enter marks of subject 2:");
+            float sub2 = sc.nextFloat();
+
+            System.out.println("Enter marks of subject 3:");
+            float sub3 = sc.nextFloat();
+
+            System.out.println("Enter marks of subject 4:");
+            float sub4 = sc.nextFloat();
+
+            System.out.println("Enter marks of subject 5:");
+            float sub5 = sc.nextFloat();
+
+            float total = sub1+sub2+sub3+sub4+sub5;
+
+            float percentage = (total / 500)* 100;
+
+
+            System.out.println("Total marks = " + total);
+            System.out.println("percentage = "+ percentage + "%");
+
+
+
+
+        }
+
+    }
+
